0dc96b46e064505f452845e94f33c341cf5efc89
AndreusSH <andreamammoliti@yahoo.it>
2023-07-17 18:13:15 +0200

    new commit

--- js/callDataFunctionm.mjs
+++ js/callDataFunctionm.mjs
@@ -1,11 +1,21 @@
 const url = "./json/senate.json";
+const url_2 = "./json/congress.json";
 let first_name;
+let selected_party;
+let target;
+let resultElement;
+
+const response2 = await fetch(url);
+const data2 = await response.json();
 
  const getData = async () => {
+   //console.log("trigger the data function");
   try {
     const response = await fetch(url);
     const data = await response.json();
+    const members = await  data?.result;
     //first_name = data.results[0].members[0].first_name;
+    console.log(members,'test')
     return data;
     //first_name;
   } catch (error) {
@@ -14,32 +24,72 @@ let first_name;
 };
 
 
-const getFirstName = async () => {
-    const data = await getData();
-    
-      //console.log(typeof data.results)
-      //console.log(data.results[0].members[1].title)
-
-      //first_name = data.results[0].members[0].first_name;  
-      console.log(data.results[0].members);  
-      for (let obj of data.results[0].members) {
-       first_name = obj.first_name;
-       const target = document.getElementById('name');
-       const resultElement = document.createElement('p');
-       resultElement.textContent = first_name;
-       target.appendChild(resultElement);
-
-       
-      }
-    
-
-       
-    }
  
-    getFirstName();
+
+//buildtable
+
+const click = () =>{
+   let checkboxes = document.querySelectorAll(".checkbox"); 
+    
+   checkboxes.forEach(function(checkbox) {
+     checkbox.addEventListener("change", function() {
+       //console.log("-------------------some input-------------------")
+       checkboxes.forEach(function(checkbox) {
+         if (checkbox.checked) {
+         //console.log("checked")
+          selected_party = checkbox.value;
+          //console.log( typeof selected_party);
+          
+         }
+         getFirstName(selected_party);
+      })
+     })
+ 
+   })
+ 
+}
+
+
+
+const getFirstName = async (selected_party) => {
+    const data = await getData();
+
+     // Filter by party
+ 
+      
+            //console.log(party);
+
+            for (let obj of data.results[0].members) {
+               //faccio troppi loop e ripeto gli stessi nomi. 
+                if(selected_party == obj.party){
+                   first_name = obj.first_name;
+                  //console.log(first_name);
+                   target = document.getElementById('name');
+                   resultElement = document.createElement('p');
+                  resultElement.textContent = first_name;
+                  target.appendChild(resultElement);
+                }
+            
+            }
+            
+              }
+            
+       
     
 
+     
+ 
+    //getFirstName();
+    click();
+
+   
+ 
+  
+       
+   
 
 
+
+ 
 
 
---
+++ js/getSenatenames.mjs
@@ -0,0 +1,7 @@
+//import { getData } from './js';
+//console.log(getData())
+import { data } from "../json/senate.mjs";
+
+
+
+console.log(data,'ciao andrea')
