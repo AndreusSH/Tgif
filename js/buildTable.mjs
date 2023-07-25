5f80acabe65f1c341b0d3ae5f41ecd12dd56c267
AndreusSH <andreamammoliti@yahoo.it>
2023-07-25 20:17:28 +0200

    message when the table does not display anything

--- js/buildTable.mjs
+++ js/buildTable.mjs
@@ -6,37 +6,47 @@ export const buildTable =  (arr) => {
    //console.log(arr,'data1')
     document.getElementById("header").innerHTML = ""; 
      let table = document.getElementById("header");
-       
-  
-  //loop through all members of the chamber or senate
-  for (let i = 0 ; i < arr.length; i++){
-     
-     let row = table.insertRow() // Add a new row to the table
-
-  
-  
-    // Add cells to the new row
-    let name = row.insertCell(0);
-    let party = row.insertCell(1);
-    let state = row.insertCell(2);
-    let years_office = row.insertCell(3);
-    let votes = row.insertCell(4);
-
-
-    // Add a link around the name
-    const link = document.createElement("a");
-    link.href = arr[i].url;
-    link.textContent = arr[i].first_name + " " + arr[i].last_name;
-    name.appendChild(link);
-
-      //name.innerHTML =  arr[i].first_name + " " + arr[i].last_name;
       
-      party.innerHTML = arr[i].party;
-      state.innerHTML = arr[i].state;
-      years_office.innerHTML = arr[i].seniority;
-      votes.innerHTML = arr[i].votes_with_party_pct;
-  
-   }
+     if (arr.length == 0){
+
+       
+        
+      table.innerText = "There are no " + document.querySelector("h2").textContent + " with these filters";
+      
+      
+     }
+     else{
+ //loop through all members of the chamber or senate
+ for (let i = 0 ; i < arr.length; i++){
+     
+   let row = table.insertRow() // Add a new row to the table
+
+
+
+  // Add cells to the new row
+  let name = row.insertCell(0);
+  let party = row.insertCell(1);
+  let state = row.insertCell(2);
+  let years_office = row.insertCell(3);
+  let votes = row.insertCell(4);
+
+
+  // Add a link around the name
+  const link = document.createElement("a");
+  link.href = arr[i].url;
+  link.textContent = arr[i].first_name + " " + arr[i].last_name;
+  name.appendChild(link);
+
+    //name.innerHTML =  arr[i].first_name + " " + arr[i].last_name;
+    
+    party.innerHTML = arr[i].party;
+    state.innerHTML = arr[i].state;
+    years_office.innerHTML = arr[i].seniority;
+    votes.innerHTML = arr[i].votes_with_party_pct;
+
+ }
+     }
+ 
   
   
   }
